d3394b82e0326da23e28419f33d7df6eaa65f288
Withnu.$itksc <witsanu.sr@ksu.ac.th>
2019-02-23 11:55:32 +0700

    Initial commit

---
+++ app/src/main/java/withnu/itksc/withnuprojectfriend/MainActivity.java
@@ -0,0 +1,21 @@
+package withnu.itksc.withnuprojectfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+
+    } // Main Method
+
+} // Main Class
