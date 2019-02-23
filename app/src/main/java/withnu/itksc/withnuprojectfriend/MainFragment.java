bb31cfd6f312de8018df756ff9c17dc74e71afd8
Withnu.$itksc <witsanu.sr@ksu.ac.th>
2019-02-23 14:46:06 +0700

    Arrow Navigator Success

--- app/src/main/java/withnu/itksc/withnuprojectfriend/MainFragment.java
+++ app/src/main/java/withnu/itksc/withnuprojectfriend/MainFragment.java
@@ -2,10 +2,12 @@ package withnu.itksc.withnuprojectfriend;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -16,8 +18,29 @@ public class MainFragment extends Fragment {
 
     public MainFragment() {
         // Required empty public constructor
-    }
+    } // Constructor
 
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Register Controller
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Replace Fragment
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment, new RegisterFragment()).addToBackStack(null).commit();
+
+
+            }
+        });
+
+
+
+    } // Main Method
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,4 +49,6 @@ public class MainFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
-}
+
+
+} // Main Class
